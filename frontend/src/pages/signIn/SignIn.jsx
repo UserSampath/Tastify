12e6af9353608fa1e53b3a9161f31ed81f4c5394
IsHaNinfo <ishaninfo56@gmail.com>
2024-01-12 21:55:16 +0530

    renew the login signup

--- frontend/src/pages/signIn/SignIn.jsx
+++ frontend/src/pages/signIn/SignIn.jsx
@@ -6,6 +6,7 @@ import validator from "validator";
 import { useNavigate } from "react-router-dom";
 import Swal from "sweetalert2";
 import axios from "axios";
+import Food from "../../../image/Food.png";
 const SignIn = () => {
   const navigate = useNavigate();
   const [email, setEmail] = useState("");
@@ -70,13 +71,16 @@ const signInButtonClicked = async() => {
         <div className="signInBox">
           <div className="centerSignIn">
             <div className="signInContainer">
-              <h2>Sign In</h2>
+              <div className="adminLog">
+              <h2>ADMIN LOGIN</h2>
+              </div>
               <TextInput
                 type={"text"}
                 icon={"mail"}
-                inputName={"Email"}
-                placeholder={"Enter Email"}
+                placeholder={" Email"}
                 value={email}
+                inputName={"Email"}
+
                 onChange={(value) => setEmail(value)}
                 errorMessage={emailError}
                 onFocus={() => setEmailError("")}
@@ -84,8 +88,9 @@ const signInButtonClicked = async() => {
               <TextInput
                 type={"password"}
                 icon={"lock"}
+                placeholder={"Password"}
                 inputName={"Password"}
-                placeholder={"Enter Password"}
+
                 value={password}
                 onChange={(value) => setPassword(value)}
                 errorMessage={passwordError}
@@ -101,7 +106,6 @@ const signInButtonClicked = async() => {
               </div>
               <div className="newHereContainer">
                 <div>
-                  <h2>New Here?</h2>
                   <div className="newHereButton">
                     <Button
                       onClick={() => navigate("/register")}
@@ -115,15 +119,15 @@ const signInButtonClicked = async() => {
           </div>
         </div>
 
-        <div className="imageContainer">
+        <div className="imageContainerSignIn">
           <div className="imageBox">
             <div>
               <div className="imgContainerText">
                 <h2 className="whiteH2">Welcome to</h2>
-                <h1>Employee Management System</h1>
+                <h2 className="whiteH2"> Tastyfy Admin Panel</h2>
                 <p>Log in to access your account. </p>
               </div>
-              <img className="signInImage" src="../../image/pic1.png" alt="" />
+              <img className="signInImage" src={Food} alt="" />
             </div>
           </div>
         </div>
